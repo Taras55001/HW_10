35ae21e106aaef9c93fc6bc312babfd45dbdc61e
Taras55001 <taras5500@gmail.com>
2023-04-02 20:34:14 +0200

    v0.0.2

--- phone_parser.py
+++ phone_parser.py
@@ -3,7 +3,7 @@ from collections import UserDict
 contacts_list = {"test": [12345], "test2": [12345,]}
 command_list = "'add' - додає новий контакт\n"\
     "'change' - змінює контакт\n"\
-    "'phone' - показує конакт за ім'ям\n"\
+    "'delete' - видаляє конакт за ім'ям\n"\
     "'show' - показує список конактів\n"\
     "'good', 'bye', 'close', 'exit' - вихід з бота"
 input_line = "-" * 50 + "\n"\
@@ -17,8 +17,8 @@ class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
 
-    def change_record(self, record):
-        self.data.get()
+    def delete_record(self, name):
+        self.data.pop(name)
 
 
 class Name:
@@ -49,6 +49,19 @@ class Phone(Name):
 book = AddressBook()
 
 
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            print("Контакт не знайдено")
+        except ValueError:
+            print("Номер телефону повинен містити тільки цифри")
+        except IndexError:
+            print("Недостатньо аргументів")
+    return wrapper
+
+
 def add_record(command):
     if spliting_arguments(command):
         record = spliting_arguments(command)
@@ -57,18 +70,22 @@ def add_record(command):
         return False
 
 
+def delete(command):
+    argument = command.strip().split()[1]
+    if argument:
+        book.delete_record(argument)
+    return argument
+
+
+@input_error
 def parse_command(command):
-    try:
-        name = command.strip().split()[1]
-        phone = command.strip().split()[2]
-        return name, phone
-    except IndexError:
-        print("Не вказані дані контакту")
+    key, name, phone = command.strip().split()
+    return key, name, phone
 
 
 def spliting_arguments(command):
     try:
-        arg1, arg2 = parse_command(command)
+        key, arg1, arg2 = parse_command(command)
         name = Name(arg1)
         phone = Phone(arg2)
         return Record(name, phone)
@@ -85,14 +102,16 @@ def main():
             print("Невідома команда")
         elif command.startswith("hello"):
             return print("How can I help you?")
-        elif spliting_arguments(command) and command.startswith("add") or command.startswith("change"):
+        elif command.startswith("add") or command.startswith("change"):
             add_record(command)
-            if command.startswith("add"):
+            if parse_command(command):
                 print(
-                    f"Контакт {parse_command(command)[0]} з номером {parse_command(command)[1]} збережено")
-            else:
+                    f"Контакт {parse_command(command)[1]} з номером {parse_command(command)[2]} збережено")
+            elif parse_command(command):
                 print(
-                    f"Номер телефону для контакту {parse_command(command)[0]} змінено на {parse_command(command)[1]}")
+                    f"Номер телефону для контакту {parse_command(command)[1]} змінено на {parse_command(command)[2]}")
+        elif command.startswith("delete"):
+            print(f"конакт за ім'ям {delete(command)} видалений")
         elif command.startswith("show"):
             for n in book.data:
                 print(str(n), book.data[n].phone)
